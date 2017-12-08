834c38a914736beb8712e6cf20b58b86acf70089
James Wood <James.Wood@moonpig.com>
2017-12-08 16:11:43 +0000

    Day 1 - first half

---
+++ 2017/Captcha/Captcha/CaptchaCracker.cs
@@ -0,0 +1,43 @@
+﻿namespace Captcha
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CaptchaCracker
+    {
+        private readonly List<int> _intList;
+
+        public CaptchaCracker()
+        {
+            _intList = new List<int>();
+        }
+
+        public int Calculate(string inputString, bool indexModifier)
+        {
+            DiscoverGroupings(inputString, indexModifier);
+            return _intList.Count > 0 ? _intList.Sum() : 0;
+        }
+
+        private void DiscoverGroupings(string inputString, bool indexModifier)
+        {
+            var charArray = inputString.ToCharArray();
+            var currentIndex = 0;
+
+            while (charArray.Length > currentIndex)
+            {
+                var nextIndex = currentIndex == charArray.Length - 1 ? 0 : currentIndex + 1;
+                AddIndexToList(inputString, currentIndex, nextIndex);
+
+                currentIndex++;
+            }
+        }
+
+        private void AddIndexToList(string inputString, int currentIndex, int nextIndex)
+        {
+            if (string.Equals(inputString[currentIndex].ToString(), inputString[nextIndex].ToString()))
+            {
+                _intList.Add(int.Parse(inputString[currentIndex].ToString()));
+            }
+        }
+    }
+}
---
+++ 2017/Captcha/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("314cf643-8877-4a3f-a938-21cb925f4ace")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
