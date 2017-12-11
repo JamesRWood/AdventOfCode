4a640cf842c380f24821281649e2940d1069e024
James Wood <James.Wood@moonpig.com>
2017-12-11 13:52:06 +0000

    Day 2 - improvements

--- 2017/CheckSum/CheckSum/Checksum.cs
+++ 2017/CheckSum/CheckSum/Checksum.cs
@@ -38,18 +38,16 @@
                     var combinations = inp.SelectMany((value, index) => inp.Skip(index + 1), (first, second) => new { first, second }).ToList();
                     foreach (var c in combinations)
                     {
-                        if (c.first % c.second == 0)
+                        var result = c.first % c.second == 0 ? c.first / c.second : 
+                                     c.second % c.first == 0 ? c.second / c.first : 
+                                     0;
+
+                        if (result == 0)
                         {
-                            sumList.Add(c.first / c.second);
-                            break;
+                            continue;
                         }
 
-                        if (c.second % c.first != 0)
-                        {
-                            continue;                            
-                        }
-
-                        sumList.Add(c.second / c.first);
+                        sumList.Add(result);
                         break;
                     }
                 }
