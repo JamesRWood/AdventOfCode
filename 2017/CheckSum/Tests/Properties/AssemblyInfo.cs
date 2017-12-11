b91a8a578c03a25814f97fa92fb2dcf4a34e9d8e
James Wood <James.Wood@moonpig.com>
2017-12-11 10:39:33 +0000

    Day 2 - Initial commit

---
+++ 2017/CheckSum/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02ed7e5d-bfce-4c93-bf3b-3ece53a6ba42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 2017/CheckSum/Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
