6d7d82de6bd5dc2fe8e5abf4ae7ba5729eec6264
James Wood <James.Wood@moonpig.com>
2017-12-11 11:41:16 +0000

    Day 2 - first half

---
+++ 2017/CheckSum/CheckSum/Checksum.cs
@@ -0,0 +1,35 @@
+﻿namespace CheckSum
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class Checksum
+    {
+        public int CalculateChecksumValue(string inputFilePath)
+        {
+            return Calculate(GetInput(inputFilePath));
+        }
+
+        private static List<List<int>> GetInput(string inputFilePath)
+        {
+            var inputList = new List<List<int>>();
+            var fileReader = new StreamReader(inputFilePath);
+            while (!fileReader.EndOfStream)
+            {
+                var currentRow = fileReader.ReadLine()?.Split(null).Select(int.Parse).ToList();
+                inputList.Add(currentRow);
+            }
+
+            return inputList;
+        }
+
+        private static int Calculate(List<List<int>> input)
+        {
+            var sumList = new List<int>();
+            input.ForEach(x => sumList.Add(x.Max() - x.Min()));
+
+            return sumList.Sum();
+        }
+    }
+}
---
+++ 2017/CheckSum/Tests/ChecksumTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests
+{
+    using System.IO;
+    using CheckSum;
+    using Xunit;
+
+    public class ChecksumTests
+    {
+        private readonly Checksum _checksum;
+
+        public ChecksumTests()
+        {
+            _checksum = new Checksum();
+        }
+
+        [Fact]
+        public void Test()
+        {
+            RuntTest("Input_Test.txt", 18);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            RuntTest("Input.txt", 36174);
+        }
+
+        private void RuntTest(string fileName, int expectedOutput)
+        {
+            var filePath = Path.GetFullPath(fileName);
+
+            var result = _checksum.CalculateChecksumValue(filePath);
+
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+}
